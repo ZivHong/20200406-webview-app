419a3b03a25cfa250644c470901c45673c2e1041
hong <rock33332@gmail.com>
2020-04-22 20:22:34 +0800

    add anti-wuhan

--- app/src/main/java/com/example/myapp1/MainActivity.java
+++ app/src/main/java/com/example/myapp1/MainActivity.java
@@ -1,56 +1,42 @@
 package com.example.myapp1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     WebView webView;
-    EditText editurl;
-    Button gopage_btn;
-    ProgressBar progressWeb;
     ImageButton prev_page_btn,forw_page_btn;
     String defaulturl="https://www.google.com";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editurl = findViewById(R.id.edit_url);
-        gopage_btn = findViewById(R.id.go_btn);
-        webView = findViewById(R.id.web1);
         prev_page_btn = findViewById(R.id.previous_btn);
         forw_page_btn = findViewById(R.id.forward_btn);
-        progressWeb  = findViewById(R.id.progressBar);
         toolbar = findViewById(R.id.toolbar);
-//載入預設網址(google)
+        webView = findViewById(R.id.web1);
+        //設定toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //設定webview
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //載入預設網址(google)
         webView.loadUrl(defaulturl);
-
-        gopage_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                final int status =(Integer) v.getTag();
-                //在網頁title跟輸入網址中做切換
-                if(status == 1) {//載入網頁
-                    String url = editurl.getText().toString();
-                    webView.loadUrl(url);
-                    gopage_btn.setText("enter");
-                    v.setTag(0);
-                } else {//切換成title，且設定不可輸入
-                    gopage_btn.setText("go");
-                    v.setTag(1);
-                    editurl.setFocusableInTouchMode(true);
-                }
-            }
-        });
         //前一頁
         prev_page_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,27 +55,31 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        webView.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                progressWeb.setVisibility(View.VISIBLE);
-                progressWeb.setProgress(newProgress);
-                //設定進度條
-                if(newProgress==100){
-                    progressWeb.setVisibility(View.GONE);
-                }
-            }
-        });
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                //載入網頁結束時，設定網址列為html title
-                gopage_btn.setText("enter");
-                gopage_btn.setTag(0);
-                editurl.setFocusable(false);
-                editurl.setText(view.getTitle());
-            }
-        });
+        //TODO 儲存登入資訊
+
+    }
+    //設定toolbar menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //TODO 選單功能
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.user_login:
+                Toast.makeText(this, "登入", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.user_logout:
+                Toast.makeText(this, "登出", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about_program:
+                ShowMsgDialog("關於程式","Author : Hong Ju Dong");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
     //允許實體按鍵返回前一頁
     @Override
@@ -99,6 +89,21 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+    //跳出對話方塊
+    private void ShowMsgDialog(String title,String Msg)
+    {
+        AlertDialog.Builder MyAlertDialog = new AlertDialog.Builder(this);
+        MyAlertDialog.setTitle(title);
+        MyAlertDialog.setMessage(Msg);
+        DialogInterface.OnClickListener OkClick = new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which) {
+        //如果不做任何事情 就會直接關閉 對話方塊
+            }
+        };;
+        MyAlertDialog.setNeutralButton("關閉",OkClick );
+        MyAlertDialog.show();
     }
     //防止webview crach
     @Override
